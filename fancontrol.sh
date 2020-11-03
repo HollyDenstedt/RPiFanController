585295d3c0de83288a645aff975ddb0c6a85989b
Holly Denstedt <63131767+HollyDenstedt@users.noreply.github.com>
2020-11-02 21:37:39 -0500

    Add files via upload
    move the file to /etc/init.d to make it executable 
    
    run this to register the script: 
    sudo update-rc.d fancontrol.sh defaults

---
+++ fancontrol.sh
@@ -0,0 +1,17 @@
+case "$1" in
+  start)
+    echo "Starting fancontrol.py"
+    /usr/local/bin/fancontrol.py &
+    ;;
+  stop)
+    echo "Stopping fancontrol.py"
+    pkill -f /usr/local/bin/fancontrol.py
+    ;;
+  *)
+    echo "Usage: /etc/init.d/fancontrol.sh {start|stop}"
+    exit 1
+    ;;
+esac
+
+exit 0
+
